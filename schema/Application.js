2edd0c9f966eba1083311832b07de7aaa5331ae8
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-07-06 10:40:49 -0700

    removed hard-coded data schema

--- schema/Application.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const ApplicationSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  question1: { type: String, required: true },
-  question2: { type: String, required: true },
-});
-
-export default mongoose.model('Application', ApplicationSchema);
--- schema/User.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const UserSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
-
-export default mongoose.model('User', UserSchema);
