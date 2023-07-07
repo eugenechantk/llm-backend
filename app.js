0af56d4cb0d36b810d630b2ecf69e55e8cbbeb84
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-07-07 15:55:44 -0700

    added MongoDB API call to the dynamic BE

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ app.all("/*", async function (req, res) {
   try {
     const params = Object.values(req.params); // Get an array of all params
     const endpoint = `/${params.join("/")}`; // Join the params with '/'
-    console.log(endpoint);
     const body = req.body;
 
     const bodyString = JSON.stringify(body, null, 2);
@@ -42,46 +41,65 @@ app.all("/*", async function (req, res) {
     const userPrompt = getUserPrompt(endpoint, bodyString, existingSchema);
 
     const chatCompletion = await openai.createChatCompletion({
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       messages: [
         { role: "system", content: systemPrompt },
         { role: "user", content: userPrompt },
       ],
     });
     const response = chatCompletion.data.choices[0].message.content;
+    console.log(response);
 
     // PARSE THE RESPONSE
     // Extract API route
-    const apiRouteRegex = /!API route!: (.*?) (.*)/s;
+    const apiRouteRegex = /!API route!:(.+?)\n/m;
     const apiRouteMatch = response.match(apiRouteRegex);
-    const httpMethod = apiRouteMatch ? apiRouteMatch[1].trim() : null;
-    const remainingRoute = apiRouteMatch ? apiRouteMatch[2].trim() : null;
-
-    // Extract API headers
-    const apiHeadersRegex = /!API headers!:(.*)/s;
-    const apiHeadersMatch = response.match(apiHeadersRegex);
-    const apiHeaders = apiHeadersMatch
-      // ? JSON.parse(apiHeadersMatch[1].trim())
-      // : null;
+    const apiRoute = apiRouteMatch[1].trim();
+    const [httpMethod, remainingRoute] = apiRoute.split(" ");
 
     // Extract API body
-    const apiBodyRegex = /!API body!:(.*)/s;
+    const apiBodyRegex = /!API body!:([\s\S]+?)\n\n/;
     const apiBodyMatch = response.match(apiBodyRegex);
-    const apiBody = apiBodyMatch
-      // ? JSON.parse(apiBodyMatch[1].trim()) 
-      // : null;
+    const apiBody = apiBodyMatch ? JSON.parse(apiBodyMatch[1].trim()) : null;
 
     // Extract Updated Schema
-    const updatedSchemaRegex = /!Updated schema!:(.*)/s;
+    const updatedSchemaRegex = /!Updated schema!:([\s\S]+)/;
     const updatedSchemaMatch = response.match(updatedSchemaRegex);
     const updatedSchema = updatedSchemaMatch
-      // ? JSON.parse(updatedSchemaMatch[1].trim())
-      // : null;
-    
-    console.log(httpMethod, remainingRoute, apiHeaders, apiBody, updatedSchema)
-    
-    res.send(response);
-    res.end();
+      ? JSON.parse(updatedSchemaMatch[1].trim())
+      : null;
+
+    console.log(
+      `LLM generated request:\nmethod: ${httpMethod}\nendpoint: ${remainingRoute}\nbody: ${JSON.stringify(
+        apiBody
+      )}\nupdatedSchema: ${JSON.stringify(updatedSchema)}\n`
+    );
+    // MAKE THE MONGODB CALL
+    const mongoAPIEndpoint = `${process.env.MONGO_DB_URL}${remainingRoute}`;
+    const mongoAPIHeaders = {
+      "Content-Type": "application/json",
+      "api-key": process.env.MONGO_DB_API_KEY,
+    };
+    await fetch(mongoAPIEndpoint, {
+      method: httpMethod,
+      headers: mongoAPIHeaders,
+      body: JSON.stringify(apiBody),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // WRITE THE SCHEMA BACK TO FILE
+        fs.writeFile(schemaPath, JSON.stringify(updatedSchema), (error) => {
+          if (error) {
+            console.error('Error writing to file:', error);
+          } else {
+            console.log('Text has been written to the file successfully.');
+          }
+        });
+        res.send(data);
+        res.end();
+      });
   } catch (err) {
     console.log(err);
     res.status(500).send({ error: err });
