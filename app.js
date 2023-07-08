f75e7f0b40880fa063e62deb21768fafdf9d5674
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-07-07 19:28:15 -0700

    modify how to parse the LLM response into route, body and schema

--- app.js
+++ app.js
@@ -52,28 +52,42 @@ app.all("/*", async function (req, res) {
 
     // PARSE THE RESPONSE
     // Extract API route
-    const apiRouteRegex = /!API route!:(.+?)\n/m;
-    const apiRouteMatch = response.match(apiRouteRegex);
-    const apiRoute = apiRouteMatch[1].trim();
-    const [httpMethod, remainingRoute] = apiRoute.split(" ");
+    const apiHeadersRegex = /!API route!:([^]+)!API body!:/;
+    const apiHeadersMatch = response.match(apiHeadersRegex);
+    const endpointRegex = /\/.*/;
+    const apiEndpoint = apiHeadersMatch[1].trim().match(endpointRegex);
+    const remainingRoute = apiEndpoint ? apiEndpoint[0] : null;
 
     // Extract API body
-    const apiBodyRegex = /!API body!:([\s\S]+?)\n\n/;
+    const apiBodyRegex = /!API body!:([\s\S]*?)!Updated schema!:/;
     const apiBodyMatch = response.match(apiBodyRegex);
-    const apiBody = apiBodyMatch ? JSON.parse(apiBodyMatch[1].trim()) : null;
+    const apiBody = apiBodyMatch
+      ? JSON.parse(
+          apiBodyMatch[1]
+            .trim()
+            .replace(/^```json/, "")
+            .replace(/```$/, "")
+        )
+      : null;
 
     // Extract Updated Schema
     const updatedSchemaRegex = /!Updated schema!:([\s\S]+)/;
     const updatedSchemaMatch = response.match(updatedSchemaRegex);
     const updatedSchema = updatedSchemaMatch
-      ? JSON.parse(updatedSchemaMatch[1].trim())
+      ? JSON.parse(
+          updatedSchemaMatch[1]
+            .trim()
+            .replace(/^```json/, "")
+            .replace(/```$/, "")
+        )
       : null;
 
     console.log(
-      `LLM generated request:\nmethod: ${httpMethod}\nendpoint: ${remainingRoute}\nbody: ${JSON.stringify(
+      `LLM generated request:\nendpoint: ${remainingRoute}\nbody: ${JSON.stringify(
         apiBody
       )}\nupdatedSchema: ${JSON.stringify(updatedSchema)}\n`
     );
+    
     // MAKE THE MONGODB CALL
     const mongoAPIEndpoint = `${process.env.MONGO_DB_URL}${remainingRoute}`;
     const mongoAPIHeaders = {
@@ -81,7 +95,7 @@ app.all("/*", async function (req, res) {
       "api-key": process.env.MONGO_DB_API_KEY,
     };
     await fetch(mongoAPIEndpoint, {
-      method: httpMethod,
+      method: "POST",
       headers: mongoAPIHeaders,
       body: JSON.stringify(apiBody),
     })
@@ -92,9 +106,9 @@ app.all("/*", async function (req, res) {
         // WRITE THE SCHEMA BACK TO FILE
         fs.writeFile(schemaPath, JSON.stringify(updatedSchema), (error) => {
           if (error) {
-            console.error('Error writing to file:', error);
+            console.error("Error writing to file:", error);
           } else {
-            console.log('Text has been written to the file successfully.');
+            console.log("Text has been written to the file successfully.");
           }
         });
         res.send(data);
